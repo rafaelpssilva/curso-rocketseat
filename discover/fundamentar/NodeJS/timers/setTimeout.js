5bb9a0284eef956d0b1da88acf42328a7acd53a9
Rafael Pereira Souto Da Silva <rafaelsccp0906@gmail.com>
2022-02-28 13:16:32 -0300

    Cap de Timers finalizado

---
+++ discover/fundamentar/NodeJS/timers/setTimeout.js
@@ -0,0 +1,7 @@
+// setTimeout rodar uma função depois de X milissegundos
+
+const timeOut = 3000
+const finished = () => console.log('Done!')
+
+setTimeout(finished, timeOut)
+console.log('Mostrar')
